12fb41f34705fd8d37e8df39eca9ab80b472b968
Edward-Precious Omegbu <eomegbu@gmail.com>
2023-01-19 21:54:39 +0100

    x-robots-tag: index

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview' || process.env.NEXT_PUBLIC_VERCEL_ENV === 'production') {
+      headers.push({
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'index',
+          },
+        ],
+        source: '/:path*',
+      });
+    }
+    return headers;
+  },
 }
 
 module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -85,6 +85,8 @@ export default function Home() {
         <meta name="description" content="I'm Codegiyu, a Frontend React Developer. I build beautiful, fun and interactive things with code. Programmer portfolio website built with NextJS and Tailwind CSS." />
         <meta name="google-site-verification" content="mVKUksXdW1OhGyyAi4-iO8CT6zjQTo4uOK8GFwQUI8A" />
         <meta name="robots" content="index, follow" />
+        <meta name="keywords" content="codegiyu, portfolio, react, developer, frontend" />
+        <meta name="author" content="Edward-Precious Omegbu Codegiyu" />
         <meta property="og:type" content="portfolio" />
         <meta property="og:title" content="CodeGiyu - React Developer" />
         <meta property="og:description" content="I'm Codegiyu, a Frontend React Developer. I build beautiful, fun and interactive things with code. Programmer portfolio website built with NextJS and Tailwind CSS." />
