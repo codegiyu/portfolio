b02fd07fcff1dc67ded153e84ce2f0184a51affb
Edward-Precious Omegbu <eomegbu@gmail.com>
2023-05-19 17:19:30 +0100

    slides background image

--- components/WorkSingle.jsx
+++ components/WorkSingle.jsx
@@ -7,8 +7,8 @@ const WorkSingle = ({props}) => {
         <div className="w-full flex-none flex flex-col rounded-[20px] bg-dark text-center text-ash font-montserrat 
             overflow-hidden pb-8"
         >
-            <div className="w-full aspect-[1.8] relative">
-                <Image src={ img } alt={title} fill loading="eager" priority />
+            <div className={`w-full aspect-[1.8] relative bg-cover bg-center`} style={{ backgroundImage: `url(${img})` }}>
+                {/* <Image src={ img } alt={title} fill loading="eager" priority /> */}
             </div>
             <a href={ link }><h3 className="text-red text-2xl lg:text-[32px] hover:underline hover:underline-offset-8 font-bold my-4">{ title }</h3></a>
             <p className="text-sm lg:text-base font-semibold">{ description }</p>
